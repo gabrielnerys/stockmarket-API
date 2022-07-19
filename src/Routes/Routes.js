4f4ea921b0a19b900bc4b094e6b59752cc1b28ea
gabrielnerys <gabe.nery@live.com>
2022-07-19 19:27:13 -0300

    feat: Adds Withdraw Route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -6,5 +6,6 @@ const Clients = require('../controllers/clients.controller');
 routes.get('/conta/clientes', Clients.getAll);
 routes.get('/conta/:codCliente', Clients.getClientByCode);
 routes.post('/conta/deposito', Clients.newDeposit);
+routes.post('/conta/saque', Clients.newWithdraw);
 
 module.exports = routes;
