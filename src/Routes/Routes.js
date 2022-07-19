b8389ae314a9b22a95e1f801314cc1665bd73dcd
gabrielnerys <gabe.nery@live.com>
2022-07-19 19:11:30 -0300

    feat: Adds Deposit Route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -5,6 +5,6 @@ const Clients = require('../controllers/clients.controller');
 
 routes.get('/conta/clientes', Clients.getAll);
 routes.get('/conta/:codCliente', Clients.getClientByCode);
-
+routes.post('/conta/deposito', Clients.newDeposit);
 
 module.exports = routes;
