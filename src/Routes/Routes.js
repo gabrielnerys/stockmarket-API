3cfdf2f361be677e08d0b7d11446395959f3a5d0
gabrielnerys <gabe.nery@live.com>
2022-07-19 19:53:15 -0300

    feat: Adds Find Assets by Client Code Route

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const routes = express.Router();
 
 const Clients = require('../controllers/clients.controller');
+const Assets = require('../controllers/assets.controller');
 
 routes.get('/conta/clientes', Clients.getAll);
 routes.get('/conta/:codCliente', Clients.getClientByCode);
 routes.post('/conta/deposito', Clients.newDeposit);
 routes.post('/conta/saque', Clients.newWithdraw);
+
+routes.get('/ativos/idCliente/:codCliente', Assets.findByClientCode);
 
 module.exports = routes;
