397f44af408f91a9e4378fccdab06e463211fb4f
gabrielnerys <gabe.nery@live.com>
2022-07-19 18:47:34 -0300

    feat: Adds route that returns all Customers

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
+const Clients = require('../controllers/clients.controller');
 
+routes.get('/conta/clientes', Clients.getAll);
 
 module.exports = routes;
