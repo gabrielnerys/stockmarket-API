999eeee0046cf8cb0f476914002e5d9eaea6cf05
gabrielnerys <gabe.nery@live.com>
2022-07-19 20:48:33 -0300

    feat: Adds Route that returns all assets

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,11 +4,12 @@ const routes = express.Router();
 const Clients = require('../controllers/clients.controller');
 const Assets = require('../controllers/assets.controller');
 
-routes.get('/conta/clientes', Clients.getAll);
+// routes.get('/conta/clientes', Clients.getAll);
 routes.get('/conta/:codCliente', Clients.getClientByCode);
 routes.post('/conta/deposito', Clients.newDeposit);
 routes.post('/conta/saque', Clients.newWithdraw);
 
+routes.get('/ativos', Assets.getAll);
 routes.get('/ativos/idCliente/:codCliente', Assets.findByClientCode);
 routes.get('/ativos/idAtivo/:codAtivo', Assets.findByAssetCode);
 
