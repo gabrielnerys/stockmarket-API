a89122b46006cc4e16dd788257b46ee404822c47
gabrielnerys <gabe.nery@live.com>
2022-07-19 19:50:36 -0300

    feat: Adds getAll function to Assets Controller

--- src/controllers/assets.controller.js
+++ src/controllers/assets.controller.js
@@ -1,16 +1,9 @@
-// const Assets = require('../services/clients.service');
+const Assets = require('../services/assets.service');
 
-// const getAll = async (_req, res) => {
-//   const allAssets = await Assets.getAll();
-//   if (!allAssets) return res.status(500).json({ message: 'Não foi possível completar sua solicitação' });
-//   return res.status(200).json(allAssets);
-// };
+const getAll = async (_req, res) => {
+  const allAssets = await Assets.getAll();
+  if (!allAssets) return res.status(400).json({ message: 'Não foi possível completar sua solicitação' });
+  return res.status(200).json(allAssets);
+};
 
-// const findByClientCode = async (req, res) => {
-//   const { codCliente } = req.params
-//   const clientByCode = await Assets.findByClientCode(codCliente);
-//   if (!clientByCode) return res.status(500).json({ message: 'Cliente não encontrado' });
-//   return res.status(200).json(clientByCode);
-// };
-
-// module.exports = { getAll, findByClientCode };
+module.exports = { getAll };
