f13543bf78b98dad5fdc85c5e6f6ba50c38ef2b3
gabrielnerys <gabe.nery@live.com>
2022-07-19 18:54:11 -0300

    feat: Adds route that returns Account info by client code

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -4,5 +4,7 @@ const routes = express.Router();
 const Clients = require('../controllers/clients.controller');
 
 routes.get('/conta/clientes', Clients.getAll);
+routes.get('/conta/:codCliente', Clients.getClientByCode);
+
 
 module.exports = routes;
