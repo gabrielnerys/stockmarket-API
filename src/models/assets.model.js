b8819ab18272c53c087fde4589591e38bdc59abd
gabrielnerys <gabe.nery@live.com>
2022-07-19 19:52:41 -0300

    feat: Adds findByClientCode function to Assets model

--- src/models/assets.model.js
+++ src/models/assets.model.js
@@ -6,4 +6,20 @@ const getAll = async () => {
   return allClients;
 }
 
-module.exports = { getAll };
+const findByClientCode = async (codCliente) => {
+  const query = `
+  SELECT c.cod_cliente AS CodCliente,
+  c.cod_ativo AS CodAtivo, 
+  ROUND(SUM(c.qtde_comprada)/2) - ROUND(SUM(v.qtde_vendida)/2) AS qtdeAtivo,
+  a.valor_unit AS Valor
+  FROM StockMarketAPI.Compra AS c
+  INNER JOIN StockMarketAPI.Venda AS v ON c.cod_cliente = v.cod_cliente
+  INNER JOIN StockMarketAPI.Ativos AS a ON a.cod_ativo = c.cod_ativo
+  WHERE c.cod_cliente = ?
+  GROUP BY c.cod_cliente, c.cod_ativo;`;
+  const [request] = await connection.execute(query, [codCliente]);
+  return request;
+};
+
+
+module.exports = { getAll, findByClientCode };
