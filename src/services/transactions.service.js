18175b6c494eda699523fd7b586ce8a0ca2099b9
gabrielnerys <gabe.nery@live.com>
2022-07-19 22:41:25 -0300

    feat: Adds getAll Sales and getAll Purchases in Transations service

--- src/services/transactions.service.js
+++ src/services/transactions.service.js
@@ -1,7 +1,11 @@
 const Transactions = require('../models/transactions.model');
 
+const getAllPurchases = () => Transactions.getAllPurchases(); 
+
+const getAllSales = () => Transactions.getAllSales(); 
+
 const insertPurchase = (codCliente, codAtivo, qtdeAtivo) => Transactions.insertPurchase(codCliente, codAtivo, qtdeAtivo);
 
 const insertSale = (codCliente, codAtivo, qtdeAtivo) => Transactions.insertSale(codCliente, codAtivo, qtdeAtivo);
 
-module.exports = { insertPurchase, insertSale };
+module.exports = { getAllSales, getAllPurchases, insertPurchase, insertSale };
