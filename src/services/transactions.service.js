3b17bc0b1d93a566e50561b2276eb66b5aaefc8d
gabrielnerys <gabe.nery@live.com>
2022-07-19 22:22:27 -0300

    feat: Adds Transactions service and InsertPurchase function

--- src/services/transactions.service.js
+++ src/services/transactions.service.js
@@ -1,5 +1,7 @@
 const Transactions = require('../models/transactions.model');
 
+const insertPurchase = (codCliente, codAtivo, qtdeAtivo) => Transactions.insertPurchase(codCliente, codAtivo, qtdeAtivo);
+
 const insertSale = (codCliente, codAtivo, qtdeAtivo) => Transactions.insertSale(codCliente, codAtivo, qtdeAtivo);
 
-module.exports = { insertSale };
+module.exports = { insertPurchase, insertSale };
