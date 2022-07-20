14f7b31d516b4e45af14ae98faeb443a4874d531
gabrielnerys <gabe.nery@live.com>
2022-07-20 11:01:07 -0300

    feat: Add Purchase Validation

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -5,6 +5,8 @@ const Clients = require('../controllers/clients.controller');
 const Assets = require('../controllers/assets.controller');
 const Transactions = require('../controllers/transactions.controller');
 
+const { validatePurchase } = require('../middlewares/purchase.validation')
+
 // routes.get('/conta/clientes', Clients.getAll);
 routes.get('/conta/:codCliente', Clients.getClientByCode);
 routes.post('/conta/deposito', Clients.newDeposit);
@@ -14,7 +16,7 @@ routes.get('/ativos', Assets.getAll);
 routes.get('/ativos/idCliente/:codCliente', Assets.findByClientCode);
 routes.get('/ativos/idAtivo/:codAtivo', Assets.findByAssetCode);
 
-routes.post('/investimentos/comprar', Transactions.insertPurchase);
+routes.post('/investimentos/comprar', validatePurchase, Transactions.insertPurchase);
 routes.post('/investimentos/vender', Transactions.insertSale);
 
 routes.get('/transacoes/compra', Transactions.getAllPurchases);
---
+++ src/middlewares/purchase.validation.js
@@ -0,0 +1,19 @@
+const Assets = require('../services/assets.service')
+
+const validatePurchase = async (req, res, next) => {
+  const { codCliente, codAtivo, qtdeAtivo } = req.body;
+  const getAssetByCode = await Assets.findByAssetCode(codAtivo);
+
+  if (!codCliente) return res.status(400).json({ message: `O campo 'codCliente' não pode estar vazio.`});
+  if (!codAtivo) return res.status(400).json({ message: `O campo 'codAtivo' não pode estar vazio.`});
+  if (!qtdeAtivo) return res.status(400).json({ message: `O campo 'qtdeAtivo' não pode estar vazio.`});
+  if (!getAssetByCode) return res.status(404).json({ message: `O ativo de código ${codAtivo} não foi encontrado.`});
+
+  if(qtdeAtivo > getAssetByCode.qtdeAtivo) {
+    return res.status(400).json({ message: `Quantidade solicitada para compra é maior que a quantidade disponivel.`})
+  }
+
+  next();
+}
+
+module.exports = { validatePurchase }
