08fe0ba6f6499e8aa917fadc7b32e666f4b052aa
gabrielnerys <gabe.nery@live.com>
2022-07-20 17:53:33 -0300

    feat: Create Token Validation File

---
+++ src/middlewares/token.validation.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const SECRET = process.env.JWT_SECRET;
+
+const jwtConfig = {
+  expiresIn: '7d',
+  algorithm: 'HS256',
+};
+
+const authenticateToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não encontrado' });
+  }
+
+  try {
+    jwt.verify(token, SECRET, jwtConfig);
+  } catch (err) {
+    return res.status(401).json({ message: 'Token inválido ou expirado' });
+  }
+
+  next();
+};
+
+module.exports = { authenticateToken };
