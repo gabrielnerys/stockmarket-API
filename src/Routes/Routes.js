1fe62d84e19ae35d9c949de971c6b8f679ab3a5a
gabrielnerys <gabe.nery@live.com>
2022-07-20 17:56:46 -0300

    feat: Adds POST /login route and Token Validation Middleware

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -5,22 +5,27 @@ const Clients = require('../controllers/clients.controller');
 const Assets = require('../controllers/assets.controller');
 const Transactions = require('../controllers/transactions.controller');
 
+const { depositValidation, withdrawValidation } = require('../middlewares/transactions.validation');
 const { validatePurchase } = require('../middlewares/purchase.validation')
 const { validateSale } = require('../middlewares/sale.validation');
+const { loginValidation, authGenerator } = require('../middlewares/login.validation');
+const { authenticateToken } = require('../middlewares/token.validation');
 
-// routes.get('/conta/clientes', Clients.getAll);
-routes.get('/conta/:codCliente', Clients.getClientByCode);
-routes.post('/conta/deposito', Clients.newDeposit);
-routes.post('/conta/saque', Clients.newWithdraw);
+routes.post('/login', loginValidation, authGenerator)
+
+// routes.get('/conta/clientes', authenticateToken, Clients.getAll);
+routes.get('/conta/:codCliente', authenticateToken, Clients.getClientByCode);
+routes.post('/conta/deposito', authenticateToken, depositValidation, Clients.newDeposit);
+routes.post('/conta/saque', authenticateToken, withdrawValidation, Clients.newWithdraw);
 
 routes.get('/ativos', Assets.getAll);
-routes.get('/ativos/idCliente/:codCliente', Assets.findByClientCode);
-routes.get('/ativos/idAtivo/:codAtivo', Assets.findByAssetCode);
+routes.get('/ativos/idCliente/:codCliente', authenticateToken, Assets.findByClientCode);
+routes.get('/ativos/idAtivo/:codAtivo', authenticateToken, Assets.findByAssetCode);
 
-routes.post('/investimentos/comprar', validatePurchase, Transactions.insertPurchase);
-routes.post('/investimentos/vender', validateSale, Transactions.insertSale);
+routes.post('/investimentos/comprar', authenticateToken, validatePurchase, Transactions.insertPurchase);
+routes.post('/investimentos/vender', authenticateToken, validateSale, Transactions.insertSale);
 
-routes.get('/transacoes/compra', Transactions.getAllPurchases);
-routes.get('/transacoes/venda', Transactions.getAllSales);
+routes.get('/transacoes/compra', authenticateToken, Transactions.getAllPurchases);
+routes.get('/transacoes/venda', authenticateToken, Transactions.getAllSales);
 
 module.exports = routes;
