50807536d4b1bd89751ebf71976f1db6227e2746
gabrielnerys <gabe.nery@live.com>
2022-07-19 22:41:11 -0300

    feat: Adds getAll Sales and getAll Purchases in Transations controller

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -1,5 +1,17 @@
 const Transactions = require('../services/transactions.service');
 
+const getAllPurchases = async (_req, res) => {
+  const allPurchases = await Transactions.getAllPurchases();
+  if (!allPurchases) return res.status(400).json({ message: 'Não foi possível completar sua solicitação' });
+  return res.status(200).json(allPurchases);
+};
+
+const getAllSales = async (_req, res) => {
+  const allSales = await Transactions.getAllSales();
+  if (!allSales) return res.status(400).json({ message: 'Não foi possível completar sua solicitação' });
+  return res.status(200).json(allSales);
+};
+
 const insertPurchase = async (req, res) => {
   const { codCliente, codAtivo, qtdeAtivo } = req.body;
   const newPurchase = await Transactions.insertPurchase(codCliente, codAtivo, qtdeAtivo);
@@ -14,4 +26,4 @@ const insertSale = async (req, res) => {
   return res.status(200).json({ message: 'Venda realizada com sucesso' });
 }
 
-module.exports = { insertPurchase, insertSale };
+module.exports = { getAllSales, getAllPurchases, insertPurchase, insertSale };
