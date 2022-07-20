345a222bc3341b2f7d195be4fced8d41b8cab2da
gabrielnerys <gabe.nery@live.com>
2022-07-19 22:22:34 -0300

    feat: Adds Transactions model and InsertPurchase function

--- src/models/transactions.model.js
+++ src/models/transactions.model.js
@@ -1,9 +1,15 @@
 const connection = require('./connection');
 
+const insertPurchase = async (codCliente, codAtivo, qtdeAtivo) => {
+  const query = `INSERT INTO StockMarketAPI.Compra (cod_cliente, cod_ativo, qtde_comprada) values (?, ? ,?)`;
+  const newSale = await connection.execute(query, [codCliente, codAtivo, qtdeAtivo]);
+  return newSale;
+}
+
 const insertSale = async (codCliente, codAtivo, qtdeAtivo) => {
   const query = `INSERT INTO StockMarketAPI.Venda (cod_cliente, cod_ativo, qtde_vendida) values (?, ? ,?)`;
   const newSale = await connection.execute(query, [codCliente, codAtivo, qtdeAtivo]);
   return newSale;
 }
 
-module.exports = { insertSale };
+module.exports = { insertPurchase ,insertSale };
