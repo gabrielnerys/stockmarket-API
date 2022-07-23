7a9c57ee0844d71d78681b33f56fcb9d37e992a1
gabrielnerys <gabe.nery@live.com>
2022-07-23 00:07:18 -0300

    feat: Adds change to Swagger's JWT validation

--- src/middlewares/token.validation.js
+++ src/middlewares/token.validation.js
@@ -7,6 +7,28 @@ const jwtConfig = {
   algorithm: 'HS256',
 };
 
+// Autenticação JWT para Swagger
+
+// const authenticateToken = async (req, res, next) => {
+//   const token = req.headers.authorization;
+
+//   if (!token) {
+//     return res.status(401).json({ message: 'Token não encontrado' });
+//   }
+
+//   const [, auth] = token.split(' ');
+
+//   try {
+//     jwt.verify(auth, SECRET, jwtConfig);
+//   } catch (err) {
+//     return res.status(401).json({ message: 'Token inválido ou expirado' });
+//   }
+
+//   next();
+// };
+
+// Autenticação padrão para Postman
+
 const authenticateToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
