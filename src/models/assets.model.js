136a7ad22fa55553c44593faf96d32cbc8b98e44
gabrielnerys <gabe.nery@live.com>
2022-07-23 10:03:39 -0300

    feat: Modify query that returns all assets

--- src/models/assets.model.js
+++ src/models/assets.model.js
@@ -2,12 +2,23 @@ const connection = require('./connection');
 
 const getAll = async () => {
   const query = `
-  SELECT cod_ativo AS CodAtivo,
-  simbolo AS Simbolo,
-  nome_ativo AS NomeAtivo,
-  valor_unit AS Valor,
-  qtde_ativo AS QtdeAtivo 
-  FROM StockMarketAPI.Ativos;`;
+  SELECT c.cod_ativo AS CodAtivo, 
+  Ativos.simbolo AS Simbolo,
+  Ativos.nome_ativo AS NomeAtivo,
+  c.soma - v.soma AS QtdeAtivo, 
+  Ativos.valor_unit AS Valor
+  FROM (
+  SELECT SUM(Compra.qtde_comprada) AS soma, 
+  Compra.cod_ativo, 
+  Compra.cod_cliente FROM StockMarketAPI.Compra 
+  GROUP BY Compra.cod_ativo, Compra.cod_cliente) AS c
+  INNER JOIN (
+  SELECT SUM(Venda.qtde_vendida) as soma, 
+  Venda.cod_ativo, Venda.cod_cliente FROM StockMarketAPI.Venda 
+  GROUP BY Venda.cod_ativo, Venda.cod_cliente) AS v
+  ON (v.cod_ativo = c.cod_ativo AND v.cod_cliente = c.cod_cliente)
+  INNER JOIN StockMarketAPI.Ativos ON (Ativos.cod_ativo = c.cod_ativo)
+  ORDER BY c.cod_cliente;`;
   const [allAssets] = await connection.execute(query);
   return allAssets;
 }
