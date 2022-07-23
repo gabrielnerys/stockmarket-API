64edbe65c445e7c88384aa0efe7d9c5c0e037890
gabrielnerys <gabe.nery@live.com>
2022-07-23 11:52:27 -0300

    feat: Add cors to the project

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
+const cors = require('cors');
 const app = require('./app');
 
 const { PORT } = process.env;
 
+app.use(cors());
 app.get('/', (_request, response) => {
   response.send();
 });
