d37fb9a974c1020e1ea9ba8a1fed3e139d9a7888
gabrielnerys <gabe.nery@live.com>
2022-07-23 13:44:02 -0300

    feat: Change JWT validation to Swagger

--- src/middlewares/token.validation.js
+++ src/middlewares/token.validation.js
@@ -9,26 +9,6 @@ const jwtConfig = {
 
 // Autenticação JWT para Swagger
 
-// const authenticateToken = async (req, res, next) => {
-//   const token = req.headers.authorization;
-
-//   if (!token) {
-//     return res.status(401).json({ message: 'Token não encontrado' });
-//   }
-
-//   const [, auth] = token.split(' ');
-
-//   try {
-//     jwt.verify(auth, SECRET, jwtConfig);
-//   } catch (err) {
-//     return res.status(401).json({ message: 'Token inválido ou expirado' });
-//   }
-
-//   next();
-// };
-
-// Autenticação padrão para Postman ou Insomnia
-
 const authenticateToken = async (req, res, next) => {
   const token = req.headers.authorization;
 
@@ -36,13 +16,33 @@ const authenticateToken = async (req, res, next) => {
     return res.status(401).json({ message: 'Token não encontrado' });
   }
 
+  const [, auth] = token.split(' ');
+
   try {
-    jwt.verify(token, SECRET, jwtConfig);
+    jwt.verify(auth, SECRET, jwtConfig);
   } catch (err) {
     return res.status(401).json({ message: 'Token inválido ou expirado' });
   }
 
   next();
 };
+
+// Autenticação padrão para Postman ou Insomnia
+
+// const authenticateToken = async (req, res, next) => {
+//   const token = req.headers.authorization;
+
+//   if (!token) {
+//     return res.status(401).json({ message: 'Token não encontrado' });
+//   }
+
+//   try {
+//     jwt.verify(token, SECRET, jwtConfig);
+//   } catch (err) {
+//     return res.status(401).json({ message: 'Token inválido ou expirado' });
+//   }
+
+//   next();
+// };
 
 module.exports = { authenticateToken };
